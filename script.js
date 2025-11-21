d4be218fe8f3ef6d45705087912ded746e31f1c4
doda82 <doda2598@gmail.com>
2025-11-20 21:29:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// No seu arquivo script.js
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('mobile-menu').classList.toggle('active');
+});
